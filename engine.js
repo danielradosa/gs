5d17e9026b1c6488ed0b5ba814d71f066f7e303b
Daniel Radosa <danysko5@gmail.com>
2024-08-22 09:52:45 +0200

    oki doki

---
+++ engine.js
@@ -0,0 +1,7 @@
+/* DYNAMIC CHANGES EXAMPLES */
+
+// CUSTOM GUTTER WIDTH
+document.documentElement.style.setProperty('--gutter-width', '24px');
+
+// CUSTOM SCREEN SIZE
+// document.documentElement.style.setProperty('--screen-md', '340px');
